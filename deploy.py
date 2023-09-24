5d16790f73c983b5f830bf017d93da4d2aa5b1a9
Salvatore <salvatore.botta@outlook.com>
2023-09-24 12:06:39 +0200

    added creation of props folder

--- deploy.py
+++ deploy.py
@@ -8,6 +8,7 @@ def deploy():
 
     todaysReleaseFolderPath = createTodaysReleaseFolder()
     bkp_folder_path = createBkpFolder(todaysReleaseFolderPath)
+    bkp_folder_path = createFoldersForBkpFolder(bkp_folder_path)
     release_folder_path = createReleaseFolder(todaysReleaseFolderPath)
     moveReleaseToNewlyCreatedFolder(release_folder_path)
 
@@ -44,6 +45,15 @@ def createBkpFolder(path):
     return bkp_folder_path_name
 
 
+def createFoldersForBkpFolder(bkpFolderPath):
+    bkp_FE_folder_path = os.mkdir(bkpFolderPath + "/FE")
+    bkp_BE_folder_path = os.mkdir(bkpFolderPath + "/BE")
+    bkp_PROPS_folder_path = os.mkdir(bkpFolderPath + "/PROPS")
+    paths = {"BKP_FE_FOLDER_PATH": bkp_FE_folder_path,
+             "BKP_BE_FOLDER_PATH": bkp_BE_folder_path, "BKP_PROPS_FOLDER_PATH": bkp_PROPS_folder_path}
+    return paths
+
+
 def createReleaseFolder(path):
     release_folder_name = "/release"
     release_folder_path_name = path + release_folder_name
@@ -54,11 +64,15 @@ def createReleaseFolder(path):
 def moveReleaseToNewlyCreatedFolder(destinationFolderPath):
     try:
         desktopPath = os.path.expanduser("~/Desktop")
-        folder_to_cut = "/rel"
-        source_folder = desktopPath + folder_to_cut
+        folder_to_copy = "/rel"
+        source_folder = desktopPath + folder_to_copy
         fe_path = moveFE(source_folder, destinationFolderPath)
         be_path = moveBE(source_folder, destinationFolderPath)
+        props_path = movePROPS(source_folder, destinationFolderPath)
+        paths = {"RELEASE_FE_FOLDER_PATH": fe_path,
+                 "RELEASE_BE_FOLDER_PATH": be_path, "RELEASE_PROPS_FOLDER_PATH": props_path}
         os.rmdir(source_folder)
+        return paths
     except shutil.Error:
         print("A folder with the same name already exists")
         return
@@ -80,6 +94,15 @@ def moveBE(sourceFolder, destinationPath):
         return movedFolderPath
     except FileNotFoundError:
         print("Error while trying to move BE folder, reason: folder not found")
+
+
+def movePROPS(sourceFolder, destinationPath):
+    folderToMovePath = sourceFolder + "/PROPS"
+    try:
+        movedFolderPath = shutil.move(folderToMovePath, destinationPath)
+        return movedFolderPath
+    except FileNotFoundError:
+        print("Error while trying to move PROPS folder, reason: folder not found")
 
 
 deploy()
